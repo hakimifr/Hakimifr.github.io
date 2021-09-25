6727c685a04034dee3cec31f4c66fab24445a31d
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-09-26 03:25:11 +0800

    0

---
+++ bruh.js
@@ -0,0 +1,19 @@
+
+
+
+let co = document.getElementById("count")
+let count = 0
+console.log(co)
+function increment() {
+    count = count + 1
+    console.log("button was pressed")
+    co.innerText = count
+}
+
+function nuke() {
+    count = 0
+    console.log("value reset")
+    co.innerText = count
+}
+
+
