a7056a29d184c229f777a0a2d31f4909dfa233e7
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-09-26 04:20:00 +0800

    114

--- bruh.js
+++ bruh.js
@@ -5,7 +5,7 @@ let co = document.getElementById("count")
 let count = 0
 console.log(co)
 function increment() {
-    count = count + 1
+    count += 1
     console.log("button was pressed")
     co.innerText = count
 }
