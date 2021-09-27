69c9e145f8c49e993a216129c5d41d01b6cde02f
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-09-27 11:02:30 +0800

    6019

--- scripts/redirect_github.js
+++ scripts/redirect_github.js
@@ -12,7 +12,7 @@ function redirect(dest) {
     } else if (dest == "discord") {
         console.log('beaming to discord html page');
         window.location.href = "../html/discord.html";
-    } 
+    } else if (dest )
     else {
         console.log('something went wrong')
     }
