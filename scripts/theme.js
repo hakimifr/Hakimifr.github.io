0a73d54256382adc93e341a68f224fbfca1b3987
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-09-28 01:26:57 +0800

    7401

---
+++ scripts/theme.js
@@ -0,0 +1,37 @@
+// kanged from stackoverflow
+// To save or update use :
+// localStorage.setItem('theme','value');
+
+// To get the Value:
+// localStorage.getItem('theme');
+
+// To clear:
+// localStorage.clear()
+
+function darktheme() {
+    document.getElementById('bg').style.backgroundColor = 'gray';
+}
+
+function lighttheme() {
+    document.getElementById('bg').style.backgroundColor = '#ff92bc88'
+}
+
+var theme = localStorage.getItem('theme');
+console.log(theme);
+var change = document.getElementById('bg');
+function changetheme(color) {
+    if (color == "dark") {
+        darktheme();
+    } else if (color == "light") {
+        lighttheme();
+    } else {}
+}
+
+if (theme == "dark") {
+    darktheme();
+} else if (theme == "light") {
+    lighttheme();
+} else {}
+
+
+
