749bbe3fceb61581c9a6e8e854b88404131e4afd
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-09-27 11:00:00 +0800

    6000

---
+++ scripts/redirect_github.js
@@ -0,0 +1,18 @@
+// window.location.href = "https://github.com/Hakimi0804";
+
+function redirect(dest) {
+    if (dest == "telegram") {
+        console.log('beaming to telegram')
+        window.location.href = "https://t.me/Hakimi0804";
+    } else if (dest == "github") {
+        console.log('beaming to github')
+        window.location.href = "https://github.com/Hakimi0804";
+    } else if (dest == "twitter") {
+        console.log('beaming to twitter')
+    } else if (dest == "discord") {
+        console.log('beaming to discord html page');
+        window.location.href = "../html/discord.html";
+    } else {
+        console.log('something went wrong')
+    }
+}
