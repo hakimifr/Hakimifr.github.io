9d638caf986c81bdae7fced94c67d1316c3e9e2d
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-09-28 10:57:56 +0800

    8558

--- scripts/theme.js
+++ scripts/theme.js
@@ -16,7 +16,7 @@
 function darktheme() {
     document.getElementById('bg').style.backgroundColor = '#212121';
     document.getElementById('bg').style.color = '#c9c9c9';
-    document.get
+    document.getElementsByTagName('button').s
     localStorage.setItem('theme', 'dark')
 }
 
