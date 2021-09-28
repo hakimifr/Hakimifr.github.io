07c49ef3b3169698c3937fb86462b9153ebf0f32
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-09-28 11:01:24 +0800

    8588

--- scripts/theme.js
+++ scripts/theme.js
@@ -17,7 +17,7 @@ function darktheme() {
     document.getElementById('bg').style.backgroundColor = '#212121';
     document.getElementById('bg').style.color = '#c9c9c9';
     document.getElementById('count-ttl').style.color = '#c9c9c9';
-    document.getElementById('count-txt').style
+    document.getElementById('count-txt').style.color = '#c9c9c9';
     localStorage.setItem('theme', 'dark')
 }
 
