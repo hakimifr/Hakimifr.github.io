4fa8831d3430b5c53c0fd4b0c3cddb4c5f68ec96
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-12-11 22:08:28 +0800

    16575

---
+++ scripts/second.js
@@ -0,0 +1,17 @@
+
+document.getElementById("sum-el").textContent = ptext;
+// document.getElementById("num1").textContent = num1;
+// document.getElementById("num2").textContent = num2;
+
+let num1 = 3
+let num2 = 3
+
+function add() {
+    let result = num1 + num2;
+    ptext.textContent = "sum: " + result;
+}
+
+/**
+ * some weird problem going on here,
+ * idk will fix later
+ */
