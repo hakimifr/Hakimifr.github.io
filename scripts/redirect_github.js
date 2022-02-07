8120bc15f9f0d7423a15e71c1ebb1affff1513d8
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-02-08 03:12:35 +0800

    16928

--- scripts/redirect_github.js
+++ scripts/redirect_github.js
@@ -1,24 +1,6 @@
 // window.location.href = "https://github.com/Hakimi0804";
 
 function redirect(dest) {
-    if (dest == "telegram") {
-        console.log('beaming to telegram')
-        window.location.href = "https://t.me/Hakimi0804";
-    } else if (dest == "github") {
-        console.log('beaming to github')
-        window.location.href = "https://github.com/Hakimi0804";
-    } else if (dest == "twitter") {
-        console.log('beaming to twitter')
-    } else if (dest == "discord") {
-        console.log('beaming to discord html page');
-        window.location.href = "../html/discord.html";
-    } else if (dest == "back") {
-        console.log('bacc')
-        window.location.href = "../index.html";
-    } else {
-        console.log('something went wrong')
-    }
-
     switch (dest) {
         case "telegram":
             window.location.href = "https://t.me/Hakimi0804";
@@ -42,6 +24,5 @@ function redirect(dest) {
             break;
         default:
             console.log('something went wrong');
-
     }
 }
