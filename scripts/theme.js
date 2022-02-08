2e3fab677f313102c49f690a58d3f3cea26d7880
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-02-08 08:53:50 +0800

    20284

--- scripts/theme.js
+++ scripts/theme.js
@@ -18,7 +18,7 @@
 // }
 function darktheme() {
     document.getElementById('bg').style.backgroundColor = 'black';
-    document.getElementById('reload').style.color =;
+    document.getElementById('reload').style.color = 'white';
     localStorage.setItem('theme', 'dark')
 }
 
