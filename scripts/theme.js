04b6fd882c2465fb672d3f0cde177526b2f63ed2
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-11 19:11:41 +0800

    24064

--- scripts/theme.js
+++ scripts/theme.js
@@ -46,4 +46,10 @@ if (theme == "dark") {
 } else {}
 
 
-
+var file = location.pathname.split( "/" ).pop();
+var link = document.createElement( "link" );
+link.href = file.substr( 0, file.lastIndexOf( "." ) ) + ".css";
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+document.getElementsByTagName( "head" )[0].appendChild( link );
