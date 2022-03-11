9050a6e560433b3ec26c9ab24ed57835669f33d1
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-11 17:58:27 +0800

    23369

--- scripts/index.js
+++ scripts/index.js
@@ -5,17 +5,17 @@ console.log(co)
 function increment() {
     count += 1
     console.log("button was pressed")
-    co.innerText = count
+    co.innerText = `count: ${count}`
 }
 
 function nuke() {
     count = 0
     console.log("value reset")
-    co.innerText = count
+    co.innerText = `count: ${count}`
 }
 
 function whatever() {
     count -= 1
     console.log("decrement button was pressed")
-    co.innerText = count
+    co.innerText = `count: ${count}`
 }
